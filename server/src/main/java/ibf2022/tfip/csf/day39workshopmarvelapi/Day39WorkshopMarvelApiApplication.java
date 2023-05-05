efa73f1cd5f5d199e8a2f42274464d4d2a19dd9e
Codetatorang <102163869+Codetatorang@users.noreply.github.com>
2023-05-05 09:27:13 +0800

    inital commit

---
+++ server/src/main/java/ibf2022/tfip/csf/day39workshopmarvelapi/Day39WorkshopMarvelApiApplication.java
@@ -0,0 +1,13 @@
+package ibf2022.tfip.csf.day39workshopmarvelapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day39WorkshopMarvelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day39WorkshopMarvelApiApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/ibf2022/tfip/csf/day39workshopmarvelapi/Day39WorkshopMarvelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2022.tfip.csf.day39workshopmarvelapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day39WorkshopMarvelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
